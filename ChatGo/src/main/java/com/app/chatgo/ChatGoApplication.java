efe9d1abd46eb482f0b8bf4eadd8ebc740454b54
opi6opi6 <opi6@hanmail.net>
2022-12-07 08:25:25 +0900

    스프링

---
+++ ChatGo/src/main/java/com/app/chatgo/ChatGoApplication.java
@@ -0,0 +1,13 @@
+package com.app.chatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGoApplication.class, args);
+	}
+
+}
